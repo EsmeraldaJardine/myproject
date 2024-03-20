3005c87810cdb9dea53ac3cbf207599f994347bb
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-03-20 21:33:56 +0000

    added the models to the templates

--- pets_for_students/views.py
+++ pets_for_students/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from pets_for_students.models import Student, Cat
 
 
 def index(request):
-    context_dict = {'boldmessage': 'The Students and their cats are:'}
+    student_list = Student.objects.order_by('last_name')
+    cats_list = Cat.objects.order_by('name')
+    #cat_list = Cat.objects.order_by('num_of_cats') order by???
+    context_dict = {}
+    context_dict['boldmessage'] = 'The Students and their cats are:'
+    context_dict['students'] = student_list
+    context_dict['pet_list'] = cats_list
     return render(request, 'pets_for_students/index.html', context=context_dict)
 
+
 def cats(request):
-    context_dict = {'boldmessage': 'All the cats are:'}
+    cats_list = Cat.objects.order_by('name')
+    context_dict = {}
+    context_dict['boldmessage'] = 'All the cats are:'
+    context_dict['cats'] = cats_list
     return render(request, 'pets_for_students/cats.html', context=context_dict)
 
 
